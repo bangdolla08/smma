ba7e6fdb32d159dccb0da7f53e8e3afef3245169
bangdolla08 <bangdolla08@gmail.com>
2021-03-19 22:32:18 +0700

    Initial commit

---
+++ src/test/java/com/testcode/smma/SmmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcode.smma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
